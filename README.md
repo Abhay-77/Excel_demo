e9b15cb7fb11f29ab5925a847233d90165eeb681
Abhay Murali <abhaymuralim9@gmail.com>
2025-06-20 15:34:51 +0530

    edit

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Excel website
 
   A website to get selected for excel 2025 volunteer - web team.
+
+## 🧭 Navigation Routes – Excel 2025 Website
+
+- 🔗 `#hero`  : Hero section
+
+- 🔗 `#about` : About section
+
+- 📂 `/games` : To navigate to excel games page
+
+- 📂 `/login` : User login page
+
+- 🔗 `#dates` : Schedules section
+
+---
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,20 +5,34 @@ const NavBar = () => {
       { title: "Home", link: "#hero" },
       { title: "About", link: "#about" },
       { title: "Games", link: "/games" },
-      { title: "Dates", link: "3dates" },
+      { title: "Dates", link: "#dates" },
       { title: "Login", link: "/login" },
     ];
       
   return (
-    <nav className="bg-blue-300 text-blue-900 flex justify-between px-8 py-2 items-center h-[10vh] fixed w-full">
+    <nav className="z-50 bg-gray-600 text-neutral-300 flex justify-between px-8 py-2 items-center h-[10vh] fixed w-full">
       <img src="/logo.png" alt="logo" className="size-24 p-2" />
       <h1 className="font-semibold text-xl">Excel 2025</h1>
       <div className="flex gap-4 px-2 text-lg">
-        {navItems.map((item) => (
-          <Link to={item.link} key={item.title} className="hover:text-blue-700">
-            {item.title}
-          </Link>
-        ))}
+        {navItems.map((item) =>
+          item.link.startsWith("/") ? (
+            <Link
+              to={item.link}
+              key={item.title}
+              className="hover:text-neutral-700"
+            >
+              {item.title}
+            </Link>
+          ) : (
+            <a
+              href={item.link}
+              key={item.title}
+              className="hover:text-neutral-700"
+            >
+              {item.title}
+            </a>
+          )
+        )}
       </div>
     </nav>
   );
